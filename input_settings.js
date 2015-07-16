d1c6b1f74957313c6e6511cb9e723afe783ad826
twiddler <alex.roos@gmx.net>
2015-07-16 13:56:34 +0200

    Split settings and input from actual script

---
+++ input_settings.js
@@ -0,0 +1,6 @@
+// Allowed mismatches, relative. A threshold of 0.01 means that 1% of a fragments amino acids might be different than expected, that is 1 of 100 amino acids is altered.
+var threshold = 0.008;
+
+// Colors for the different sequence that the fragments derived from
+var fragment_colors = ['rgb(115, 154, 255)','rgb(255, 105, 105)','rgb(40, 203, 40)','rgb(255, 206, 100)','rgb(247, 119, 211)','rgb(125, 222, 239)','rgb(164, 96, 249)','rgb(244, 230, 89)','rgb(62, 240, 186)'];
+var fragment_colors_a = ['rgba(115, 154, 255, 0.3)','rgba(255, 105, 105, 0.3)','rgba(40, 203, 40, 0.3)','rgba(255, 206, 100, 0.3)','rgba(247, 119, 211, 0.3)','rgba(125, 222, 239, 0.3)','rgba(164, 96, 249, 0.3)','rgba(244, 230, 89, 0.3)','rgba(62, 240, 186, 0.3)'];
